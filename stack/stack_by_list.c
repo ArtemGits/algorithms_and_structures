2b4fd0ce5dd5c4ed0d46bad8badef83b3864cab6
pak11273 <pak11273@gmail.com>
2018-08-02 00:44:23 +0300

    new structure

---
+++ stack/stack_by_list.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+//return stack - не понятно что будет в условии в функции createstack
+//1. Struct Stack and List
+//2. create stack
+//3. push
+//4. pop
+//5. empty
+
