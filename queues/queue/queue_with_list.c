cfae0f9e644a4ddaae251bd1080d76b910a32630
pak11273 <pak11273@gmail.com>
2018-08-07 08:32:13 +0300

    queue_with list, two array alpha version

--- queues/queue/queue_with_list.c
+++ queues/queue/queue_with_list.c
@@ -2,87 +2,85 @@
 #include <stdlib.h>
 
 
-struct Queue {
-	int size
-	List *head;
-	List *tail;
-}; 
-
-struct List {
+typedef struct List {
 	int item;
-	List *next;
-};
+  struct	List *next;
+}* _List;
 
 
-Queue create_queue(void) {
-	Queue queue = malloc(sizeof(struct Queue));
-	if (!queue)
+typedef struct Queue {
+	int size;
+  struct 	List *head;
+  struct 	List *tail;
+}* _Queue; 
+
+
+_Queue create_queue(void) {
+	_Queue queue = malloc(sizeof(struct Queue));
+	//if (!queue)
 		//Debug
-		printf(queue);
-		return queue;
+	//	printf(queue);
+	//	return queue;
 	queue->head = queue->tail = NULL;
 	queue->size = 0;
 	return queue;
 }
 
 
-void enqueue(Queue queue, int item) {
-	List new_element = malloc(sizeof(struct List))
+void enqueue(_Queue queue, int item) {
+	_List new_element = malloc(sizeof(struct List));
 
 	// if empty?
 	if(!new_element)
-		return
+		return;
 
 	new_element->item = item; 
 	new_element->next = NULL;
 
 	if(queue->head) {
-		queue->head->next = new_element
+		queue->tail->next = new_element;
 	} else
-		queue->head = new_element 
+		queue->head = new_element; 
 	
 	queue->size++;
 	queue->tail = new_element;
 }
 
-int isEmpty(queue) {
-	return queue->size == 0
+int isEmpty(_Queue queue) {
+	return queue->size == 0;
 }
 
-int dequeue(queue) {
+int dequeue(_Queue queue) {
 	if (isEmpty(queue)) 
 		return -1;
 
 	int result = queue->head->item;	
-	List *mem_ptr = queue->head; 
+	_List mem_ptr = queue->head; 
 	if(queue->head == queue->tail )
 		queue->tail = NULL;
 	queue->head = queue->head->next; 
-
-	free(mem_ptr)
+	free(mem_ptr);
 	queue->size--;
-	return  result
+	return  result;
 }
 
 
 int main(int argc, char const *argv[]) {
 	/* code */
-	Queue queue = create_queue();
-	
+	_Queue queue = create_queue();	
 	int data[] = {1,2,3,4,5,6,7,8,9,10};
-	int i;
+	int i = 0;
 
 	while(i < 5) 
-		enqueue(queue,data[++i]);
+		enqueue(queue,data[i++]);
 
 	while(!isEmpty(queue))
 		printf( "%d\n", dequeue(queue));
 
 	while(i<10)
-		enqueue(queue,data[++i]);		
+		enqueue(queue,data[i++]);		
 
 	while(!isEmpty(queue))
 		printf( "%d\n", dequeue(queue));
-
 	return 0;
 }
